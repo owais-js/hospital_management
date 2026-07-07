b79bdc4f4260851161e0c8b39d0e40bc5d9a7470
owais-js <owaisshabbir3131@gmail.com>
2026-07-07 11:45:24 +0500

    model folder created and patient model updated

---
+++ app/models/patient.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from app.core.database import Base
+
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(20), nullable=False)
+    phone = Column(String(20), nullable=False)
