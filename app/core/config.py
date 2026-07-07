26bb160a4fe10a2b7b18648adad9cceec8bef1c9
owais-js <owaisshabbir3131@gmail.com>
2026-07-07 11:32:55 +0500

    config.py modified

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
